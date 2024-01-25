b2917b214a3a3e3d7b2bc7106d265bc3caf1e56a
Marek Szukalski <marszuka@gmail.com>
2024-01-25 23:30:04 +0100

    Bump gradle to >8.0

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_smartcar_auth/FlutterSmartcarAuthPlugin.h>)
+#import <flutter_smartcar_auth/FlutterSmartcarAuthPlugin.h>
+#else
+@import flutter_smartcar_auth;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterSmartcarAuthPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterSmartcarAuthPlugin"]];
+}
+
+@end
